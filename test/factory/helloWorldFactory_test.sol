cd1aaeead02fd2bd423d3df4da67f8c038f38bb1
shwndi <924238829@qq.com>
2025-07-28 02:31:15 +0800

    complated the factrory

--- test/factory/helloWorldFactory_test.sol
+++ test/factory/helloWorldFactory_test.sol
@@ -11,6 +11,25 @@ import { storage_map_test } from "../storage_map_test.sol";
 import { HelloWorld } from "https://github.com/smartcontractkit/Web3_tutorial_Chinese/blob/main/lesson-2/HelloWorld.sol";
 
 contract HelloWarldFactory{
+    storage_map_test hw;
+    storage_map_test[] hws;
+
+    function createHelloWorld() public {
+        hw = new storage_map_test();
+        hws.push(hw);
+    }
+
+    function getHelloWorlds(uint256 _index) public view returns(storage_map_test ){
+        return hws[_index];
+    }
+
+    function sayHelloWorldFromFactory(uint256 _index,uint256 _id) public view returns (string memory) {
+        return hws[_index].sayHello(_id);
+    }
+
+    function setInfosFromFactory(uint256 _index, string memory phrase, uint256 _id) public {
+        hws[_index].setInfos(phrase, _id);
+    }
 
     
 }
