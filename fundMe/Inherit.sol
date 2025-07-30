5846330bdb9d0722ccd6230430dc636c5cc502d9
shwndi <924238829@qq.com>
2025-07-30 16:34:25 +0800

    Inherit

--- fundMe/Inherit.sol
+++ fundMe/Inherit.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.30;
 
 contract Parent{
     uint256 public a;
+    uint256 private b;
+    uint256 public c;
     function addOne() public  {
         a++;
     } 
@@ -14,6 +16,13 @@ contract Childen{
     function addOne() public {
         a ++;
     }
+    function addTwo()public {
+        a += 2;
+    }
+}
+
+contract ChildenInherit is Parent{
+
     function addTwo()public {
         a += 2;
     }
